e92bcf6c87c7b3d736521e2e2d6598b1b25f3500
YuriTur <utur2006@gmail.com>
2022-10-10 15:33:14 +0300

    HW #8 JS - "click"processinf.Karpaty

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 function setDetails(anchor) {
     detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
-    detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
+    detailsTitle.innerHTML = anchor.getAttribute('data-details-title');      
 }
 for(let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function() {
