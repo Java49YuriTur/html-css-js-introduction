c9fffabf9a54fc9f07e7ba0cbdd9e37b4c7b0f3b
YuriTur <utur2006@gmail.com>
2022-10-25 18:25:35 +0300

    HW #10 add audio. Upgrade

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ const audio = document.querySelector(".details-audio");
 function setDetails(anchor) {
     detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
     detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
-
-    pause(1);
+ 
+    pause(1);                                                                    /* timer for pause */
     audio.setAttribute('src',anchor.getAttribute('data-details-audio'));
 }
 
@@ -24,25 +24,26 @@ for(let i = 0; i < anchors.length; i++) {
         
     })
 }
-function showDetails() {                /*Click on img to remove hidden */
+function showDetails() {                                             /*Click on img to remove hidden */
     mainClass.classList.remove(HIDDEN);
     detailsContainer.classList.add(IS_POINT);
     
-    setTimeout(function() {
+    setTimeout(function() {                                      /* setTimeout -    timer to run code */
         detailsContainer.classList.remove(IS_POINT);
-    },1);
+    },1);                                                       /* 1 -   waiting for removal IS_POINT*/ 
 
     play();
-    pause(100000);
+    pause(100000);                                            /* pause time */ 
+
 }
-function hideDetails() {          /* When you click the Hide-details button, delete Details-image and turn Hidden*/
+function hideDetails() {                /* When you click the Hide-details button, delete Details-image and turn Hidden*/
     audio.pause();
     mainClass.classList.add(HIDDEN);
 }
 function play() {
     setTimeout(function (){
         audio.play();
-    },1);
+    },1);                                                   /* time when the music comes on */
 }
 function pause(msecs) {
     setTimeout(function () {
