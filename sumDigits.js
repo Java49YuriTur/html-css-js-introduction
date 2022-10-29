7194201a9ceef192c1a9ad7e2e852218f0555730
YuriTur <utur2006@gmail.com>
2022-10-29 18:39:01 +0300

    HW #11 sumDigits upgrade.

--- sumDigits.js
+++ sumDigits.js
@@ -1,18 +1,13 @@
 console.log("Function - sumDigits.js");
-function sumDigits(number) {
-   
-    if(number < 0) {
-        number =- number;
-    }
-    let sum = 1;
-    for(i = 0; i <= number; i++) {
-
-        console.log("check i =", i);                // debug
-       console.log("check sum", sum);         // debug
-
+function sumDigits(number) {  
+    if(number < 0 ) {
+        number =- number;   
+    } 
+    let sum = 0;
+    while(number !== 0) {    
         sum += number % 10;
         number = Math.floor(number/10);        // floor - rounded to a smaller                                              
     }
     return sum;
 }
-console.log('SUM =', sumDigits(123467));
+console.log('SUM =', sumDigits(123456));
