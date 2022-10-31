aa1d4c5e19f000958fa223fa8e573d3787b27c71
YuriTur <utur2006@gmail.com>
2022-10-31 13:36:03 +0200

    HW #11 +

--- sumDigits.js
+++ sumDigits.js
@@ -1,13 +1,17 @@
-console.log("Function - sumDigits.js");
-function sumDigits(number) {  
-    if(number < 0 ) {
-        number =- number;   
-    } 
+//HW #11
+function sumDigits(number) {
+    //compute sum of digits for the integer numbers
+    number = Math.abs(number);    // if the number is negative then there will be a partial
     let sum = 0;
-    while(number !== 0) {    
-        sum += number % 10;
-        number = Math.floor(number/10);        // floor - rounded to a smaller                                              
-    }
+    number = Math.trunc(number); // get rid of the fractional party
+    do {
+        let digit = number % 10;
+        number = (number - digit) / 10;
+        sum += digit;
+    }while(number != 0);
     return sum;
+
 }
-console.log('SUM =', sumDigits(123456));
+// Example
+let sum = sumDigits(123);
+console.log(sumDigits(123)); //should be printed out 6
