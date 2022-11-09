32e4fee4f4a7a01de2138114fb04b7aadb797317
YuriTur <utur2006@gmail.com>
2022-11-09 15:09:25 +0200

    HW #14   - map, reduce, forEach, splice, slice.

--- app.js
+++ app.js
@@ -1,81 +1,41 @@
-// let ar = [];
-// ar[10000] = 100;
-// ar[1] = [1, 2, 3];
-// console.log("length of array = ",ar.length);
-// ar[0] = "hello";
-// console.log("10000-th element = ", ar[10000]);
-// console.log("0-th element = ", ar[0]);
-// console.log("1-th element = ", ar[1]);
-// let str = "Hello";
-// let arStr = Array.from(str); //getting array of the string's symbols
-// console.log("String 'Hello' -> array is ", arStr);
-// for (let i = 0; i < arStr.length; i++) {
-//     console.log("element at index ", i, arStr[i]);
-// }
-// function println(element, index, array) {
-//     console.log("element at index ", index, element);
-// }
-// arStr.forEach(println);
-// let arCodeAscii = arStr.map(function(symbol, index) {
-//     return index % 2 == 0 ? symbol.charCodeAt() : symbol;
-// })
-// console.log(arStr, arCodeAscii);
-// let sumAscii = arStr.reduce(function(res, cur) {
-//     return res + cur.charCodeAt();
-// }, 0)
-// console.log("sum of ascii ", sumAscii)
-// console.log(arStr.reduce(function(res, cur) {
-// return res + cur
-// }, ""));
-function getNumberOddIndex(element) {
-    let res = element * 2;
-    if (res > 9) {
-        res -= 9;
-    }
-    return res;
-}
-function getCurrentNumber(element, index) {
-    return index % 2 == 0 ? +element : getNumberOddIndex(element);
-}
-function getArrayForSum(teudatStrNumber) {
-    let array = Array.from(teudatStrNumber);
-    return array.map(getCurrentNumber);
-}
-function getSum(array) {
-    // let res = 0;
-    // for (let i = 0; i < array.length; i++) {
-    //     res += array[i];
-    // }
-    return array.reduce(function (res, cur) {
-        return res + cur;
+///////////////////////Task #1////////////////////////
+let strings =["abc","lmn","cd"];
+strings = usSurround(strings);
+function usSurround(strings) {
+    strings = strings.map(function(element, ind) {
+        return '<li>' + element + '</li>';
+    });
+    strings.splice(0, 0, '<ul>');
+    strings.splice(4, 0, '</ul>');
+    return strings;     
+} 
+// Test :
+console.log(strings);
+
+
+
+///////////////////////Task #2///////////////////////////
+
+let strings1 = ["abc", "lmn", "cd", "abc", "abc"];
+function count(strings1, s) {
+    return strings1.reduce(function(count, element) {
+        return element === s ? count +1 : count;
     }, 0);
 }
+// Test :
+console.log('abc=', count(strings1, 'ab'));
 
-function checkTeudatZehut(teudatStrNumber) {
-    //TODO
-    //control sum of for even index digit value, for odd index digit * 2
-    //control sum should be divide on 10 with no remainder
-    //example 123456782 => 1 + 4 +3 + 8 +5 + 3 + 7 + 7 + 2 => true
-    //    123456783 => 1 + 4 +3 + 8 +5 + 3 + 7 + 7 + 3 => false
-    let res = false;
-    if (teudatStrNumber.length == 9 && !isNaN(teudatStrNumber)) {
-        let arrayForSum = getArrayForSum(teudatStrNumber);
-        let sum = getSum(arrayForSum);
-        res = sum % 10 == 0;
-    }
-    return res
 
+
+///////////////////////Task #3/////////////////////////////
+let arS = [1,2,3,4,5,6,7];
+let arD = [10,20,30,40,50,60,70,];
+arrayCopy(arS, 3, arD, 4, 3);
+function arrayCopy(src, srcPos, dst, dstPos, Length) {
+    src.slice(srcPos, srcPos + Length).reverse().forEach(element => {
+        dst.splice(dstPos, 0, element);
+    });
 }
-function generateRandomTeudatZehut() {
-    //TODO
-    //returns string of 9 symbols matching checkTeudatZehut
-    //make 8 random digits from 0 to 9
-    //9 - th symbol should be with accordance of matching
-    //to get random digit Math.round(Math.random() * 9)
-}
-// console.log(checkTeudatZehut('123456783'));
-
-let numbers = ['123456782', '1234', 'abcd123', '123456783'];
-numbers.forEach(function (e) {
-    console.log(`teudat zehut: ${e}, return of the method checkTeudatZehut: ${checkTeudatZehut(e)}`)
-});
+// Test :
+console.log('arS = ',arS);
+console.log('arD = ',arD);
