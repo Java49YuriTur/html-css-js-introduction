d39772cb6bee678bed5f288eb582cc24e21adfb3
YuriTur <utur2006@gmail.com>
2022-11-13 14:32:21 +0200

    HW #15 sort, prefix, min-max number.

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-function getEvenNumbers(numbers) {
-    let res = numbers.filter((n) => i % 2 == 0);
-    return res;
-}
-let ar = [2, 9, 3, 1999];
-ar.sort((a, b)=>a - b);
+//////////////////////////////////// Task# 1 ///////////////////////////////////////////
 
-console.log(ar);
-ar = ["abc", "a", "lmn", "y"];
-ar.sort((a,b)=>a.length - b.length);
-console.log(ar);
+function minMax(numbers) {
+    return numbers.reduce((mnmx, element) => {
+        if(element < mnmx[0]) {
+            mnmx[0] = element;
+        }
+        if(element > mnmx[1]) {
+            mnmx[1] = element;
+        }
+        return mnmx;
+    },[numbers[0],numbers[0]]);
+}
+// Test:
+let numbers = [1, 2, 3, 4, 5];
+console.log(`minMax. input: ${numbers} output: ${minMax(numbers)}`);
+
+
+////////////////////////// Task #2 /////////////////////////////////////////////
+
+function deleteWithPrefix(strings, prefix) {
+    return strings.filter(element => !element.startsWith(prefix));
+}
+//Test:
+let strings = ["abc", "old_abc", "lmn", "123", "old_lmn"];
+let prefix = "old";
+console.log(`deleteWithPrefix. input: ${strings}, prefix: ${prefix} output: ${deleteWithPrefix(strings,prefix)}`);
+
 
