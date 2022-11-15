4dfa504fbc9d99865784b16315ece1beaa0cbbab
YuriTur <utur2006@gmail.com>
2022-11-15 14:13:58 +0200

    HW #15 ++

--- app.js
+++ app.js
@@ -1,29 +1,50 @@
-//////////////////////////////////// Task# 1 ///////////////////////////////////////////
+////////////////////////// Task #1 ////////////////////////////////////////////
 
-function minMax(numbers) {
-    return numbers.reduce((mnmx, element) => {
-        if(element < mnmx[0]) {
-            mnmx[0] = element;
+// function minMax(numbers) {
+//     return numbers.reduce((res, cur) => {
+//         if (cur < res[0]) {
+//             res[0] = cur;
+//         } else if (cur > res[1]) {
+//             res[1] = cur;
+//         }
+//         return res;
+//     },[numbers[0], numbers[0]]);
+// }
+// //Test:
+// console.log("test for minMax function");
+// let array = [[1, 2, 3, 5, 100], [2, -10, 3, 1], [100, 38, 7], [10000]]
+// array.forEach(a => console.log(`inpute array: ${a}, min-max array is: ${minMax(a)}`));
+
+///////////////////////////////////////// Task #2 /////////////////////////////////
+
+// function deleteWithPrefix(strings, prefix) {
+//     return strings.filter(str => !str.startsWith(prefix))
+// }
+// let strings = ["abc", "old_abc", "lmn", "123", "old_lmn"];
+// let prefix = "old";
+// console.log(`deleteWithPrefix. input: ${strings}, prefix: ${prefix} output: ${deleteWithPrefix(strings,prefix)}`);
+
+
+////////////////////////////// Task #3 ///////////////////////////////////////////////////
+
+function getSortedEvenOdd(numbers) {
+    const result = numbers.slice();
+    return result.sort((a, b) => {
+        let res = 1;
+        if(a % 2 == 0 && b % 2 != 0) {
+            res = -1;
+        }else if(a % 2 == 0 && b % 2 == 0) {
+            res = a - b;
+        }else if (a % 2 == 1 && b % 2 == 1) {
+            res = b - a;
         }
-        if(element > mnmx[1]) {
-            mnmx[1] = element;
-        }
-        return mnmx;
-    },[numbers[0],numbers[0]]);
+         return res;
+    })
 }
 // Test:
-let numbers = [1, 2, 3, 4, 5];
-console.log(`minMax. input: ${numbers} output: ${minMax(numbers)}`);
+let numbers = [1, 6, 3, 8, 5, 2, 7, 4]
+console.log(`input: ${numbers}: output: ${getSortedEvenOdd(numbers)}`)
 
 
-////////////////////////// Task #2 /////////////////////////////////////////////
-
-function deleteWithPrefix(strings, prefix) {
-    return strings.filter(element => !element.startsWith(prefix));
-}
-//Test:
-let strings = ["abc", "old_abc", "lmn", "123", "old_lmn"];
-let prefix = "old";
-console.log(`deleteWithPrefix. input: ${strings}, prefix: ${prefix} output: ${deleteWithPrefix(strings,prefix)}`);
 
 
