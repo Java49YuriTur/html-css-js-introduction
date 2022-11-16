9184e5c6f88c6933896ab755fb59fed86f2ab3bd
YuriTur <utur2006@gmail.com>
2022-11-16 14:37:00 +0200

    HW 16   "Game - guess"

--- src/wordle.js
+++ src/wordle.js
@@ -1,24 +1,57 @@
-const word = "table"
+const letterElements = document.querySelectorAll(".letter-guess");
+const gameStatus = document.querySelector('.game-status');
+const currentTrialField = document.querySelector('.current-trial');
+
+const words = 'table';
 const N_LETTERS = 5;
-const letterElements = document.querySelectorAll(".letter-guess")
+const N_TRIALS = 6;
+const colRed = 'red';
+const colGreen = 'green';
+const colYellow = 'yellow';
+let currentTrial = 0;
+
+let word = '';
+initWord();
+
 function onChange(event) {
-    const wordGuess = event.target.value;
+    
+    const wordGuess = event.target.value.toLowerCase();
+    currentTrialField.innerHTML = N_TRIALS-currentTrial++;
     event.target.value='';
     if (wordGuess.length != N_LETTERS) {
-        alert(`A word should contain ${N_LETTERS} letters`)
+        alert(`A word should contain ${N_LETTERS} letters`);
+        
     } else {
         const wordAr = Array.from(wordGuess);
         wordAr.forEach((l, i) => letterElements[i].innerHTML = l)
         const colors = wordAr.map((l, i) => {
             let index = word.indexOf(l);
-            let res = 'red';
+            let res = colRed;
             if (index  > -1) {
-                res = index == i ? 'green' : 'yellow'
-            }
+                res = index == i ? colGreen: colYellow ;
+            }        
             return res;
         })
-        colors.forEach((c, i) =>
-         letterElements[i].style.color=c)
+        colors.forEach((c, i) => letterElements[i].style.color=c);
+        update(wordGuess, word);
     }
-   
+ }
+
+function update(wordGuess, word) {
+    currentTrialField.innerHTML = 'Remaining trials: ' + (N_TRIALS-currentTrial);
+    if(wordGuess === word) {
+        gameStatus.innerHTML = "Congratulations - you have guessed word ";
+        gameStatus.style.color = colGreen;
+        initWord();
+    } else if(currentTrial == N_TRIALS) {
+        gameStatus.innerHTML = "Sorry - your guess trials have ended up";
+        gameStatus.style.color = COLOR_RED;
+        initWord();
+    }
+}
+
+function initWord() {
+    currentTrial = 0;
+    word = words;
+    currentTrialField.innerHTML = N_TRIALS - currentTrial;
 }
