dd905e34277f4795ac96031d5beefb144d489049
YuriTur <utur2006@gmail.com>
2022-11-25 23:13:54 +0200

    HW 18 isAnagram solution

--- anagram.js
+++ anagram.js
@@ -1,46 +1,39 @@
-function OccurrencesInAnagram(word) {
-    let wordarray = Array.from(word);
-    const occurrences = {};
-    wordarray.forEach(element => {
-        if (occurrences[element]) {
-            occurrences[element]++;
-        } else {
-            occurrences[element] = 1;
-        }
-    });
-    return occurrences;
-}
-function isAnagram(word1, word2) {
-  if(word1.length != word2.length) {
-    console.log("No length");
-    return false;
+function isAnagram(str1, str2) {
+  let res = false;
+  if (str1.length == str2.length) {
+      const str1Occurrences = getSymbolOccurrences(str1);
+      res = checkAnagram(str1Occurrences, str2);
   }
-  let occurrencesInTlCase = OccurrencesInAnagram(word1.toLowerCase());
-  console.log('Before work', occurrencesInTlCase);
-  let arrW2 = Array.from(word2.toLowerCase());
-  for(let i = 0; i < arrW2.length; i++) {
-    if(occurrencesInTlCase[arrW2[i]] == undefined) {
-        console.log("letter doesn’t exist");
-      return false;
-    }
-    if(--occurrencesInTlCase[arrW2[i]] < 0) {
-      console.log('Less than - 0 ', occurrencesInTlCase);
-      return false;
-    }
-  }
-  console.log('After work', occurrencesInTlCase);
-  return true;
+  return res;
+
 }
-// TEST:
-const word = 'Yellow';
-console.log( isAnagram(word, 'weloly'));
-console.log( isAnagram(word, 'leloyw'));
-console.log( isAnagram(word, 'wolley'));
-console.log( isAnagram(word, 'weloyl'));
-console.log( isAnagram(word, 'weloll'));  
-console.log( isAnagram(word, 'leloy'));    
-console.log( isAnagram(word, 'wollet'));  
-console.log( isAnagram(word, 'weloyo'));  
+function symbolProcessing(res, cur) {
+  if (res[cur]) {
+      res[cur]++;
+  } else {
+      res[cur] = 1;
+  }
+  return res;
+}
+function getSymbolOccurrences(string) {
+  const stringArray = Array.from(string);
+  return stringArray.reduce(symbolProcessing
+      , {});
+}
 
+function checkAnagram(occurrences, string) {
+  const stringArray = Array.from(string);
+ 
+  return stringArray.every(symbol => {
+      let res = false;
+      if (occurrences[symbol]) {
+          res = true;
+          occurrences[symbol]--
+      }
+      return res;
 
+  })
+
+}
+console.log(isAnagram("yellow","wolley"));
 
