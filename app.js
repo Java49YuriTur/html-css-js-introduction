8b6fb47465bae75c5042361689a747c5aa2a44e9
YuriTur <utur2006@gmail.com>
2022-12-05 13:38:38 +0200

    CW #22 Object Oriented Introduction , Rectangle

---
+++ app.js
@@ -0,0 +1,41 @@
+// function f1() {
+//     const a = this;
+//     return a;
+// }
+// const f2 = () => {
+//     const a = this;
+//     return a;
+// }
+// const f = f1;
+// f();
+// f2();
+// f1.d = 20;
+// console.log(f1.d);
+// console.log(f1);
+// const rectangle = {
+//     width: 3,
+//     height: 5,
+//     square: function() {      
+//         return this.width * this.height;
+//     },
+//     perimeter: function() {
+//         return this.width * 2 + this.height * 2;
+//     },
+//     display: function() {
+//         console.log(`square is ${this.square()}; perimetr is ${this.perimeter()}`)
+//     }
+// }
+function Rectangle(width, height) {
+    this.width = width;
+    this.height = height;
+}
+Rectangle.prototype.square = function() {
+    return this.width * this.height;
+}
+Rectangle.prototype.perimeter = function() {
+    return this.width * 2 + this.height * 2;
+}
+const rectangle = new Rectangle(3, 5);
+console.log(rectangle.square());
+console.log(rectangle.perimeter());
+//rectangle.display();
